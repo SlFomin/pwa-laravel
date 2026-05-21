4bac73e057b60e6de538c75f34fd2cd6598154d5
Vyacheslav Brynzevich <slfomin@mail.ru>
2026-05-21 14:28:43 +0500

    feat(sw): Зарегистрирован роут pwa.sw для ServiceWorkerController

--- routes/pwa.php
+++ routes/pwa.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use SlFomin\PwaLaravel\Http\Controllers\ManifestController;
+use SlFomin\PwaLaravel\Http\Controllers\ServiceWorkerController;
 
 $route = config('pwa.manifest.route', '/manifest.webmanifest');
 
 Route::get($route, ManifestController::class)
     ->middleware(['web', 'pwa.headers'])
     ->name('pwa.manifest');
+
+Route::get(config('pwa.service_worker.url', '/sw.js'), ServiceWorkerController::class)
+    ->middleware(['pwa.headers'])
+    ->name('pwa.sw');
